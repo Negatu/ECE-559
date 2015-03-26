85e2de2a17f3be64b22de642701a23eaaff96b6e
gnyikayaramba <gnyik91@gmail.com>
2015-03-25 20:40:39 -0400

    Counter System Finished

---
+++ Receive_Port/LengthCounterFSM.vhd
@@ -0,0 +1,67 @@
+library ieee ;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_misc.all;
+
+entity LengthCounterFSM is 
+port( clk, reset_sig, crc_RDV:	in std_logic;
+	  bufferWE, CntEnable:		out std_logic); --bufferWE = buffer write enable, CntEnable = counter enable
+end LengthCounterFSM;
+
+
+architecture lengthCounterArch of LengthCounterFSM is
+	type state_type is
+				(RESET, WAITING, COUNT, WRITEOUT);
+	signal state_reg, state_next: state_type;
+	
+	begin
+		process(clk, reset_sig)  -- STATE REGISTER UPDATE
+		begin 
+			if(reset_sig = '1') then 
+				state_reg <= RESET;
+			elsif(clk'event and clk='1') then
+				state_reg <= state_next;
+			end if;
+		end process;
+
+		process(state_reg, crc_RDV) -- NEXT STATE LOGIC
+		begin
+			case state_reg is 
+				when RESET =>
+					state_next <= WAITING;
+				when WAITING =>
+					if (crc_RDV='1') then
+						state_next <= COUNT;
+					else
+						state_next <= WAITING;
+					end if;
+				when COUNT =>
+					if (crc_RDV='0') then
+						state_next <= WRITEOUT;
+					else
+						state_next <= COUNT;
+					end if;
+				when WRITEOUT =>
+					state_next <= RESET;	
+			end case;
+		end process;
+
+		process(state_reg) -- MOORE Machine
+		begin
+			case state_reg is
+				when RESET =>
+					bufferWE <= '0';
+					CntEnable <= '0';
+				when WAITING =>
+					bufferWE <= '0';
+					CntEnable <= '0';
+				when COUNT =>
+					bufferWE <= '0';
+					CntEnable <= '1';
+				when WRITEOUT =>
+					bufferWE <= '1';
+					CntEnable <= '0';
+			end case;
+		end process;
+	
+	end lengthCounterArch;
---
+++ Receive_Port/LengthCounterSystem.vhd
@@ -0,0 +1,53 @@
+-- Top level design entity for CRC_register and CRC_FSM
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_misc.all;
+
+entity LengthCounterSystem is
+	port(clock, CRC_rdv, reset: in std_logic; 
+		 lengthValid,  buffer_WE: out std_logic;
+		 lengthValue: out std_logic_vector(9 downto 0));
+end LengthCounterSystem;
+
+architecture combined of LengthCounterSystem is
+
+	component LengthCounterFSM is 
+	port(
+			clk			: IN STD_LOGIC ;
+			reset_sig	: IN STD_LOGIC ;
+			CRC_rdv			: IN STD_LOGIC ;
+			bufferWE	: OUT STD_LOGIC ;
+			CntEnable	: OUT STD_LOGIC 
+			); 
+	end component;
+	
+	component LengthCounter
+	PORT
+	(
+		aclr		: IN STD_LOGIC ;
+		clock		: IN STD_LOGIC ;
+		cnt_en		: IN STD_LOGIC ;
+		q		: OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
+	);
+	end component;
+
+	signal CntEnable_sig : std_logic;
+	
+	begin
+	
+	LengthCounterFSM_int : LengthCounterFSM PORT MAP (
+			clk			=> clock,
+			reset_sig	=> reset,
+			CRC_rdv		=> CRC_rdv,
+			bufferWE	=> buffer_WE,
+			CntEnable	=> CntEnable_sig 
+			); 
+	
+	LengthCounter_inst : LengthCounter PORT MAP (
+		aclr	 => reset,
+		clock	 => clock,
+		cnt_en	 => CntEnable_sig,
+		q	 => lengthValue
+	);
+	
+end combined;
